66f9cb1602a0fe3c628f0f2dd33a80487f012783
uo270992 <uo270992@uniovi.es>
2024-02-23 16:59:21 +0100

    Autoplay off

--- sesion5/src/prac5-1.js
+++ sesion5/src/prac5-1.js
@@ -2,4 +2,4 @@ import dashjs from "dashjs";
 
 const url = "./manifests/sintelFinal.mpd";
 const player = dashjs.MediaPlayer().create();
-player.initialize(document.querySelector("#player"), url, true);
+player.initialize(document.querySelector("#player"), url, false);
