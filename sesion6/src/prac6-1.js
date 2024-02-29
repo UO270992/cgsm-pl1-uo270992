3b3ba6dbeae82842c31db867cba054c9ffbb12c2
uo270992 <uo270992@uniovi.es>
2024-02-29 20:11:43 +0100

    sesion6

---
+++ sesion6/src/prac6-1.js
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+import adapter from 'webrtc-adapter';
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+
+if ( !WEBGL.isWebGLAvailable() ) {
+    // WebGL is available
+    throw new Error("WEBGL is not available");
+}
+
+const video = document.querySelector( 'video' );
+const constraints = {
+    audio: false,
+    video: true
+};
+
+navigator.mediaDevices.getUserMedia( constraints )
+    // Called when we get the requested streams
+    .then( ( stream ) => {
+
+        // Video tracks (usually only one)
+        const videoTracks = stream.getVideoTracks( );
+        console.log( 'Stream characteristics: ', constraints );
+        console.log( 'Using device: ' + videoTracks[0].label );
+
+        // End of stream handler
+        stream.onended = () => {
+
+            console.log( 'End of stream' );
+        };
+
+        // Bind the stream to the html video element
+        video.srcObject = stream;
+})
+    // Called in case of error
+    .catch( ( error ) => {
+
+        if ( error.name === 'ConstraintNotSatisfiedError' ) {
+
+            console.error( 'The resolution ' + constraints.video.width.exact + 'x' +
+                          constraints.video.width.exact + ' px is not supported by the camera.' );
+        } else if ( error.name === 'PermissionDeniedError' ) {
+
+            console.error( 'The user has not allowed the access to the camera and the microphone.' );
+        }
+        console.error( ' Error in getUserMedia: ' + error.name, error );
+});
---
+++ sesion6/src/prac6-4.js
@@ -0,0 +1,106 @@
+import * as THREE from 'three';
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+const clock = new THREE.Clock( );
+
+if ( !WEBGL.isWebGLAvailable() ) {
+    throw new Error("WEBGL is not available");
+}
+
+    window.addEventListener( 'resize', ( ) => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix( );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.render( scene, camera );
+    }, false );
+    // WebGL is available
+    const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer( {antialias: true} );
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    document.body.appendChild( renderer.domElement );
+    //Cámara
+    const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+    camera.position.set( 0, 0, 700 );
+    //Luz
+    const light = new THREE.PointLight(0xFFFFFF,3,0,0);
+    light.position.set( 0, 0, 500);
+    scene.add(light);
+    
+    //Listener
+    const startButton = document.getElementById('startButton');
+    startButton.addEventListener('click', () => init(), false);
+    function init() {
+        var overlay = document.getElementById('overlay');
+        overlay.remove();
+ 
+        // Do stuff
+        const video = document.getElementById('video');
+        video.play();
+    }
+
+    /*****************************************CÓDIGO**************************************** */
+
+    const video = document.getElementById( 'video' );
+    const constraints = {
+        audio: false,
+        video: { width: { exact: 640 }, height: { exact: 480 } }
+    };
+    navigator.mediaDevices.getUserMedia( constraints )
+    // Called when we get the requested streams
+    .then( ( stream ) => {
+
+        // Video tracks (usually only one)
+        const videoTracks = stream.getVideoTracks( );
+        console.log( 'Stream characteristics: ', constraints );
+        console.log( 'Using device: ' + videoTracks[0].label );
+
+        // End of stream handler
+        stream.onended = () => {
+
+            console.log( 'End of stream' );
+        };
+
+        // Bind the stream to the html video element
+        video.srcObject = stream;
+    })
+    // Called in case of error
+    .catch( ( error ) => {
+
+        if ( error.name === 'ConstraintNotSatisfiedError' ) {
+
+            console.error( 'The resolution ' + constraints.video.width.exact + 'x' +
+                          constraints.video.width.exact + ' px is not supported by the camera.' );
+        } else if ( error.name === 'PermissionDeniedError' ) {
+
+            console.error( 'The user has not allowed the access to the camera and the microphone.' );
+        }
+        console.error( ' Error in getUserMedia: ' + error.name, error );
+    });
+
+    const image = document.createElement( 'canvas' );
+    image.width = 640;  // Video width
+    image.height = 480; // Video height
+    const imageContext = image.getContext( '2d' );
+    imageContext.fillStyle = '#000000';
+    imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
+    const texture = new THREE.Texture( image );
+    const material = new THREE.MeshBasicMaterial( { map: texture } );
+    material.side = THREE.DoubleSide;
+    const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
+
+    scene.add(wall);
+    renderer.render( scene, camera );
+    animate();
+
+    function animate() {
+        const delta = clock.getDelta( ); // Elapsed time in seconds
+        const rotation = ( delta * Math.PI * 2 )/10;
+        if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+            imageContext.drawImage( video, 0, 0 );
+            if ( texture ) texture.needsUpdate = true;
+        }
+        renderer.render( scene, camera );
+
+        wall.rotation.y += rotation;
+        // Request the browser to execute the animation-rendering loop
+        requestAnimationFrame( animate );
+    };
